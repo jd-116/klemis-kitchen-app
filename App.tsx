a640f1d0b3cf7a80ca8ce95162bc7e8bdc3ab515
Twinov <hoeltgemarc@gmail.com>
2020-09-01 01:51:49 -0400

    Add React Navigation to project

--- App.tsx
+++ App.tsx
@@ -1,23 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import NativeBasePOC from './screens/NativeBasePOC'
 
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+const Stack = createStackNavigator();
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={ NativeBasePOC } />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+export default App;
