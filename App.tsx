6eb7e6323039264dbadb40f022f1f77b7da2b12f
Neil Patel <neilpatel10@yahoo.com>
2020-09-03 00:17:40 -0400

    typescripts and colors
    fixed icon and button colors as wells naming the tab Announcements

--- App.tsx
+++ App.tsx
@@ -3,17 +3,23 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createStackNavigator} from '@react-navigation/stack';
 import NativeBasePOC from './screens/NativeBasePOC'
 import HomeScreen from './screens/HomeScreen'
 
-const Drawer = createDrawerNavigator();
-const Stack = createStackNavigator();
+export type DrawerParamList = {
+  Home: undefined;
+  HomeScreen: undefined;
+  Locations: undefined;
+  Deliveries: undefined;
+};
+
+const Drawer = createDrawerNavigator<DrawerParamList>();
 
 function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName='Home'>
+      <Drawer.Navigator 
+      initialRouteName='Home'>
         <Drawer.Screen 
         name="Home" 
         component={NativeBasePOC}
