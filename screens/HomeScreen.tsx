76a714a0680a62645ff8acd28430af1ea4d16069
Neil Patel <neilpatel10@yahoo.com>
2020-09-04 18:58:41 -0400

    home screen
    with image instead of map

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { StyleSheet, Dimensions } from 'react-native';
 
-import { Text, View } from '../components/Themed';
-import MapView from 'react-native-maps'
+import { Text } from '../components/Themed';
 import {DrawerNavigationProp} from '@react-navigation/drawer';
 import {DrawerParamList} from '../App';
 
-import {  Button, Icon, Body, Title } from "native-base";
+import {  Button, Icon, Body, Title, Thumbnail, Container, Card, Content } from "native-base";
+import { useState } from 'react';
 
  type HomeScreenParamProps = {
   navigation: DrawerNavigationProp<DrawerParamList, 'HomeScreen'>
@@ -17,62 +17,51 @@ type Props = {
 }
 
 export default function HomeScreen({ navigation }: Props) {
+  const [imageURL, setImageURL] = useState('https://cdn.discordapp.com/attachments/664605666815639552/751561243978104903/iu.png')
   return (
-    <View style = {styles.container}>
-      <View style = {styles.top}>
-        <View style = {styles.backButton}>
-          <Button transparent>
-            <Icon name = 'menu' style = {{color: '#fff'}}/>
-          </Button>
-        </View>
-        <View style = {styles.searchButton}>
-          <Button transparent>
-            <Icon name = 'search'style = {{color: '#fff'}}/>
-          </Button>
-        </View>
-      </View>
-      <View style = {styles.belowTop}>
+    <Container style = {styles.container}>
+      <Container style = {styles.top}>
+        <Button transparent style = {styles.backButton}>
+          <Icon name = 'menu' style = {{color: '#fff'}}/>
+        </Button>
+        <Button transparent style = {styles.searchButton}>
+          <Icon name = 'search'style = {{color: '#fff'}}/>
+        </Button>
+      </Container>
+      <Container style = {styles.belowTop}>
           <Text style = {styles.titleName}>
           Welcome Back, _____
         </Text>
-      </View>
-      <View style = {styles.topMiddle}>
+      </Container>
+      <Container style = {styles.topMiddle}>
         <Text style = {styles.titleMap}>
            Locations Near Me
         </Text>
-      </View>
-      <View style = {styles.middle}>
-        <MapView
-                    style = {styles.MapView}
-                    initialRegion = {{
-                        latitude: 33.7759731,
-                        longitude: -84.3973371,
-                        latitudeDelta: .1,
-                        longitudeDelta: .45
-                    }}></MapView>
-        
-      </View>
-      <View style = {styles.deliveries}>
+      </Container>
+      <Container style = {styles.middle}>        
+        <Thumbnail source={{ uri: imageURL }} style = {styles.middleImage}/>
+      </Container>
+      <Container style = {styles.deliveries}>
         <Text style = {{fontSize: 18, fontWeight: 'bold', color: '#000'}}> 
           Delivery Schedule
         </Text>
         <Button style = {styles.buttons}>
-          <Text>
+          <Text style = {{fontSize: 10}}>
             View All
           </Text>
         </Button>
-      </View>
-      <View style = {styles.announcements}>
+      </Container>
+      <Container style = {styles.announcements}>
         <Text style = {{fontSize: 18, fontWeight: 'bold', color: '#000'}}> 
           Announcements
         </Text>
         <Button style = {styles.buttons}>
-          <Text>
+          <Text style = {{fontSize: 10}}>
             View All
           </Text>
         </Button>
-      </View>
-    </View>
+      </Container>
+    </Container>
   );
 }
 
@@ -86,64 +75,66 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
   top: {
-    flex: .10,
+    flex: .35,
+    width: Dimensions.get('screen').width,
+    height: (Dimensions.get('screen').height / 10),
     backgroundColor: '#83ba83',
+    borderTopWidth: 10,
+    borderTopColor: '#83ba83',
     flexDirection: 'row',
-    alignItems: 'flex-end',
     justifyContent: 'space-between',
   },
   belowTop: {
-    flex: .07,
+    flex: .2,
     backgroundColor: '#83ba83',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
   },
   titleName: {
-    flexGrow: 1,
     fontSize: 18,
     borderLeftWidth: 15,
+    borderLeftColor: 'transparent',
     width: Dimensions.get('screen').width
   },
   titleMap: {
-    flexGrow: 1,
     fontSize: 20,
     fontWeight: 'bold',
     color: '#000',
     borderLeftWidth: 15,
-    borderTopWidth: 25,
+    borderTopWidth: 10,
+    borderTopColor: 'transparent',
+    borderLeftColor: 'transparent',
     width: Dimensions.get('screen').width
   },
   backButton: {
-    flexGrow: 1,
     backgroundColor: '#83ba83',
   },
   searchButton: {
-    flexGrow: 1,
     backgroundColor: '#83ba83',
     flexDirection: 'row-reverse',
   },
   middle: {
-    flex: .35,
+    flex: 1.4,
+    width: (Dimensions.get('screen').width),
+    height: (Dimensions.get('screen').height / 2.3),
     backgroundColor: '#fff',
-    flexDirection: 'row',
+  },
+  middleImage: {
+    width: (Dimensions.get('screen').width/1.1),
+    height: (Dimensions.get('screen').height / 2.5),
+    backgroundColor: '#fff',
     alignItems: 'flex-start',
+    resizeMode: 'contain',
+    borderLeftWidth: 20,
+    borderLeftColor: 'white'
   },
   topMiddle: {
-    flex: .1,
+    flex: .24,
     backgroundColor: '#fff',
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
     alignItems: 'flex-end',
     width: Dimensions.get('screen').width
   },
-  MapView: {
-    width: (Dimensions.get('screen').width - 30),
-    height: (Dimensions.get('screen').height/3.5),
-    marginVertical: 10,
-    marginHorizontal: 15
-  },
   announcements: {
-    flex: .2,
     backgroundColor: '#fff',
     width: Dimensions.get('screen').width,
     borderRightWidth: 15,
@@ -154,18 +145,21 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   deliveries: {
-    flex: .2,
     backgroundColor: '#fff',
     width: Dimensions.get('screen').width,
     borderRightWidth: 15,
     borderRightColor: '#fff',
     borderLeftWidth: 15,
+    borderTopWidth: 15,
+    borderTopColor: '#fff',
     borderLeftColor: '#fff',
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
   buttons: {
     backgroundColor: 'rgb(235, 164, 52)',
-    padding: 15,
+    padding: (Dimensions.get('screen').width/17),
+    width: (Dimensions.get('screen').width/4),
+    height: (Dimensions.get('screen').height/20),
   },
 });
