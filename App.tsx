1785fad9e8764ea8b63a7b6e6934ed26dde73b39
Twinov <hoeltgemarc@gmail.com>
2020-09-18 21:05:33 -0400

    changed top level navigation and add greeter screen

--- App.tsx
+++ App.tsx
@@ -2,14 +2,21 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
 import { Icon } from 'native-base'
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
+import InitialScreen from './screens/InitialScreen'
 import CustomDrawerContent from './screens/Drawer'
 import NativeBasePOC from './screens/NativeBasePOC'
 import HomeScreen from './screens/HomeScreen'
 import InventoryDetails from './screens/InventoryDetails'
 import NotFoundScreen from './screens/NotFoundScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+
+export type TLSParamList = {
+  Login: undefined;
+  ActualApp: undefined; //TODO: Add in user token
+}
 
 export type DrawerParamList = {
   Home: undefined;
@@ -21,46 +28,61 @@ export type DrawerParamList = {
   Testing: undefined;
 };
 
+const TopLevelStack = createStackNavigator<TLSParamList>()
+
 const Drawer = createDrawerNavigator<DrawerParamList>();
 
 function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
-        drawerContent={props => <CustomDrawerContent {...props} />}
-        initialRouteName='Home'
-      >
-        <Drawer.Screen
-          name='Home'
-          component={HomeScreen}
-          options={{ drawerLabel: 'Home', drawerIcon: (props) => <Icon name='home' style={styles.drawerIcon} /> }}
+      <TopLevelStack.Navigator screenOptions={{ headerShown: false }} >
+        <TopLevelStack.Screen
+          name='Login'
+          component={InitialScreen}
         />
-        <Drawer.Screen
-          name='Locations'
-          component={NotFoundScreen}
-          options={{ drawerLabel: 'Locations', drawerIcon: (props) => <Icon name='map' style={styles.drawerIcon} /> }}
+        <TopLevelStack.Screen
+          name='ActualApp'
+          component={() => {
+            return (
+              <Drawer.Navigator
+                drawerContent={props => <CustomDrawerContent {...props} />}
+                initialRouteName='Home'
+              >
+                <Drawer.Screen
+                  name='Home'
+                  component={HomeScreen}
+                  options={{ drawerLabel: 'Home', drawerIcon: (props) => <Icon name='home' style={styles.drawerIcon} /> }}
+                />
+                <Drawer.Screen
+                  name='Locations'
+                  component={NotFoundScreen}
+                  options={{ drawerLabel: 'Locations', drawerIcon: (props) => <Icon name='map' style={styles.drawerIcon} /> }}
+                />
+                <Drawer.Screen
+                  name='Announcements'
+                  component={NotFoundScreen}
+                  options={{ drawerLabel: 'Announcements', drawerIcon: (props) => <Icon name='notifications' style={styles.drawerIcon} /> }}
+                />
+                <Drawer.Screen
+                  name='Deliveries'
+                  component={NotFoundScreen}
+                  options={{ drawerLabel: 'Deliveries', drawerIcon: (props) => <Icon name='cube' style={styles.drawerIcon} /> }}
+                />
+                <Drawer.Screen
+                  name='Details'
+                  component={InventoryDetails}
+                  options={{ drawerLabel: 'Details', drawerIcon: (props) => <Icon name='qr-scanner' style={styles.drawerIcon} /> }}
+                />
+                <Drawer.Screen
+                  name='Testing'
+                  component={NativeBasePOC}
+                  options={{ drawerLabel: 'Testing', drawerIcon: (props) => <Icon name='construct' style={{ color: 'red', width: 10 }} /> }}
+                />
+              </Drawer.Navigator>
+            )
+          }}
         />
-        <Drawer.Screen
-          name='Announcements'
-          component={NotFoundScreen}
-          options={{ drawerLabel: 'Announcements', drawerIcon: (props) => <Icon name='notifications' style={styles.drawerIcon} /> }}
-        />
-        <Drawer.Screen
-          name='Deliveries'
-          component={NotFoundScreen}
-          options={{ drawerLabel: 'Deliveries', drawerIcon: (props) => <Icon name='cube' style={styles.drawerIcon} /> }}
-        />
-        <Drawer.Screen
-          name='Details'
-          component={InventoryDetails}
-          options={{ drawerLabel: 'Details', drawerIcon: (props) => <Icon name='qr-scanner' style={styles.drawerIcon} /> }}
-        />
-        <Drawer.Screen
-          name='Testing'
-          component={NativeBasePOC}
-          options={{ drawerLabel: 'Testing', drawerIcon: (props) => <Icon name='construct' style={{ color: 'red', width: 10 }} /> }}
-        />
-      </Drawer.Navigator>
+      </TopLevelStack.Navigator>
     </NavigationContainer>
   );
 }
