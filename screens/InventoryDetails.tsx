b257fec1cfdb0955defa1bd24690e07d1dba2893
egu3334 <43274249+egu3334@users.noreply.github.com>
2020-09-24 04:23:25 -0400

    add to cart for inventory details

--- screens/InventoryDetails.tsx
+++ screens/InventoryDetails.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet, Dimensions, Modal, TouchableOpacity, Alert, View } from 'react-native';
 
 import { Container, Text, Button, Icon, Card, Thumbnail, Content } from 'native-base';
 import { DrawerNavigationProp } from '@react-navigation/drawer';
@@ -20,21 +20,96 @@ export default function InventoryDetailsScreen({ navigation }: Props) {
   const [itemQuantity, setItemQuantity] = useState(3)
   const [nutritionFactsLabel, setNutritionFactsLabel] = useState('http://www.scientifit.com/wp-content/uploads/2012/12/Nutrition-Facts-Label-Blondies.jpg')
 
+  const [modalVisible, setModalVisible] = useState(false);
+  const [errorMessageVisible, setErrorMessageVisible] = useState(false);
+
+  const [checkoutNumber, setCheckoutNumber] = useState(0);
+  const reset = () => setCheckoutNumber(0);
+  const increment = () => setCheckoutNumber(checkoutNumber => checkoutNumber + 1);
+  const decrement = () => setCheckoutNumber(checkoutNumber => checkoutNumber - 1)
+  if (checkoutNumber == -1) {reset()};
+
+  const showError = () => {
+    setErrorMessageVisible(true);
+    setTimeout(() => {
+      setErrorMessageVisible(false)
+    }, 1000);
+  }
+
   return (
     <Container>
+    <Modal
+        animationType = "none"
+        transparent = {true}
+        visible = {modalVisible}
+        onRequestClose={() => {
+            console.log("Modal has been closed.");
+        }}>
+      <Modal
+        animationType = "fade"
+        transparent = {true}
+        visible = {errorMessageVisible}
+        onRequestClose={() => {
+            console.log("Error message has been closed.");
+        }}>
+        <View style={styles.errorCaptureView}>
+          <View style={styles.errorMessageView}>
+            <Text style={{color: 'white'}}>Error: Not enough {itemName.toLowerCase()} at {location}.</Text>
+          </View>
+        </View>
+      </Modal>
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <Text style={styles.modalText}>How many {itemName.toLowerCase()}?</Text>
+          <View style={styles.counterView}>
+            <Button style={styles.decrementButton}
+              onPress={() => decrement()}>
+              <Text style={{ color: 'black' }}>-</Text>
+            </Button>
+            <Text style={{fontSize: 36}}>{checkoutNumber}</Text>
+            <Button style={styles.incrementButton}
+              onPress={() => increment()}>
+              <Icon name='add' style={{ color: 'black' }} />
+            </Button>
+          </View>
+          <View style = {styles.modalExitView}>
+            <Button
+              style={{backgroundColor: "white" }}
+              onPress={() => {
+                if (checkoutNumber <= itemQuantity) {
+                    setModalVisible(!modalVisible);
+                } else {
+                    showError();
+                }
+              }}>
+              <Text style={styles.textStyle}>CONFIRM</Text>
+            </Button>
+            <Button
+              style={{backgroundColor: "white" }}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+                reset();
+              }}>
+              <Text style={styles.textStyle}>CANCEL</Text>
+            </Button>
+          </View>
+        </View>
+      </View>
+    </Modal>
       <Content style={{ marginTop: 0 }}>
         <Container style={styles.buttonHeader}>
           <Button transparent onPress={() => navigation.openDrawer()}>
             <Icon name='menu' style={{ color: 'black' }} />
           </Button>
-          <Button transparent>
+          <Button transparent onPress={() => navigation.goBack()}>
             <Icon name='arrow-back' style={styles.leftArrow} />
           </Button>
         </Container>
         <Thumbnail source={{ uri: imageURL }} style={styles.itemDetailImage} />
         <Text style={styles.itemName}>{itemName}</Text>
         <Text style={styles.itemDetails}>{itemQuantity} Remaining at {location}</Text>
-        <Button bordered success style={styles.addToCartButton}>
+        <Button bordered success style={styles.addToCartButton}
+            onPress={() => {setModalVisible(true);}}>
           <Icon name='add' />
           <Text>
             Add to Cart
@@ -103,5 +178,58 @@ const styles = StyleSheet.create({
   leftArrow: {
     color: 'black',
     marginLeft: 0
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalView: {
+    margin: 50,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 20,
+    height: (Dimensions.get('screen').height * .3),
+    width: (Dimensions.get('screen').width) * .7,
+    justifyContent: "space-around"
+  },
+  textStyle: {
+    color: "green",
+    textAlign: "center"
+  },
+  modalText: {
+    textAlign: "left",
+    textAlignVertical: "top",
+    alignItems:"flex-start",
+    marginBottom: 20
+  },
+  incrementButton: {
+      backgroundColor: "white",
+      marginRight: 15
+  },
+  decrementButton: {
+      backgroundColor: "white",
+      marginLeft: 15
+  },
+  errorCaptureView: {
+      flexDirection: "column-reverse",
+      alignItems: "center",
+  },
+  errorMessageView: {
+    backgroundColor: "black",
+    height: (Dimensions.get('screen').height * .05),
+    width: (Dimensions.get('screen').width) * .9,
+    marginTop: (Dimensions.get('screen').height * .05),
+    alignItems: "center",
+    justifyContent: 'center',
+  },
+  counterView: {
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+  },
+  modalExitView: {
+      flexDirection: 'row',
+      justifyContent: 'flex-end'
   }
 });
