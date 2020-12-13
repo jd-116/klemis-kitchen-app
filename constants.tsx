da6e0319c9d82d61319fd8e74bf0b82ec243166c
Twinov <hoeltgemarc@gmail.com>
2020-12-12 21:16:46 -0500

    random images

--- constants.tsx
+++ constants.tsx
@@ -1,8 +1,8 @@
 // USE THIS TO SPECIFY A LOCATION TO GET THE API FROM
 // VALUES:
 // LOCALHOST fetches Klemis-Kitchen-API Docker Container you are running
-//  use http://localhost:8080
+//  use https://localhost:8080
 // NOTE: you will need to disable CORS for this one to work
 // PRODUCTION is for the production site
-//  use https://api.klemis-kitchen.com/v1
+//  use https://backend.klemis-kitchen.com/v1
 export const APIFETCHLOCATION = 'https://backend.klemis-kitchen.com/v1'
